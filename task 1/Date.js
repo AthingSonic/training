e12cad546fc5d58d32069e4dea08577f17d61f0e
AthingSonic <wungthing.kashungnao@sonicbolt.com>
2024-06-10 14:01:44 +0530

    Date Task

---
+++ task 1/Date.js
@@ -0,0 +1,12 @@
+const today = new Date();
+const birthday = new Date("December 17, 1995 03:24:00"); // DISCOURAGED: may not work in all runtimes
+const birthday2 = new Date("1995-12-17T03:24:00"); // This is standardized and will work reliably
+const birthday3 = new Date(1995, 11, 17); // the month is 0-indexed
+const birthday4 = new Date(1995, 11, 17, 3, 24, 0);
+const birthday5 = new Date(628021800000); // passing epoch timestamp
+console.log(`Today: ${today}`);
+console.log(`Birthday: ${birthday}`);
+console.log(`Birthday2: ${birthday2}`);
+console.log(`Birthday3: ${birthday3}`);
+console.log(`Birthday4: ${birthday4}`);
+console.log(`Birthday5: ${birthday5}`);
